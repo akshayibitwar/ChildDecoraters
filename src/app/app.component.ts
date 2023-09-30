05a65d65af7ec4359d670238ee7df64075a0172f
akshayibitwar <akshayebitwar@gmail.com>
2023-09-30 11:09:33 +0530

    ChildDecoraters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ChildDecoraters';
+  
+  todoArray : Array<string> = []; 
+  todoItem !: string;
+  @ViewChild("todoControl") todoControl !: ElementRef;
+  constructor(){}
+
+  // 1)===> by using ==>>((twowaybinding method))
+  // onTodoAdd(){
+  //   console.log(this.todoItem)   
+    // this.todoArray.push(this.todoItem)
+    // this.todoItem = '';
+   
+   // 2) ==> by using ==>>((Element-Refrence)). 
+    // onTodoAdd()
+    // let val = this.todoControl.nativeElement.value;
+    // console.log(val);
+    // this.todoArray.push(val);
+
+    // 3)==> by using Argument..
+    // onTodoAdd(todo : HTMLInputElement){
+    //   console.log(todo.value);
+    //   this.todoArray.push(todo.value);
+      
+    onTodoAdd(todo : string){
+        console.log(todo);
+        this.todoArray.push(todo);
+  
 }
+
+}
+
+
